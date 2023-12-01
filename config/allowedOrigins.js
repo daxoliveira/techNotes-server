50360d77781d686490be2de927e3eedbead2b32b
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-12-01 13:21:41 -0600

    get repo ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://www.dandrepairshop.com',
-  'https://dandrepairshop.com'
+  'https://technotes-s1li.onrender.com'
 ]
 
 module.exports = allowedOrigins;
